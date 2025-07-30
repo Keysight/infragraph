f638f0b5e31c67b80b8f4b088d25ac3bdd3a1e5b
anbalogh <andy.balogh@keysight.com>
2025-07-30 21:40:05 +0000

    fix build

--- build.py
+++ build.py
@@ -2,7 +2,6 @@
 The following command produces these artifacts:
     - ./artifacts/openapi.yaml
     - ./artifacts/openapi.json
-    - ./artifacts/openapi.html
 """
 
 import subprocess
@@ -15,4 +14,3 @@ openapiart.OpenApiArt(
     generate_version_api=False,
 ).GeneratePythonSdk(package_name="infra")
 
-subprocess.call(["redocly", "build-docs", "./artifacts/openapi.yaml", "-o", "./artifacts/openapi.html"])
